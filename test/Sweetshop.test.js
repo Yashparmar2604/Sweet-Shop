b3416b5bde58ad348d97b047a736d50afcedfbee
Yashparmar2604 <yp842694@gmail.com>
2025-07-16 17:01:15 +0530

    Test:add failing test for searching sweets by name

--- test/Sweetshop.test.js
+++ test/Sweetshop.test.js
@@ -1,73 +1,66 @@
-const {SweetShop} = require('../index.js');
+const { SweetShop } = require("../index.js");
 
-describe("SweetShop-AddSweet",()=>{
-// Test that a sweet is added successfully and returned in the sweets array
-    test('should add the sweet with valid inputs and return the sweets array',()=>{
-         const shop=new SweetShop();
-         const result=shop.addSweet("Dairy Milk","Chocolate",10,20);
-         expect(result.length).toBe(1);
-         expect(result[0].name).toBe("Dairy Milk");
+describe("SweetShop-AddSweet", () => {
+  // Test that a sweet is added successfully and returned in the sweets array
+  test("should add the sweet with valid inputs and return the sweets array", () => {
+    const shop = new SweetShop();
+    const result = shop.addSweet("Dairy Milk", "Chocolate", 10, 20);
+    expect(result.length).toBe(1);
+    expect(result[0].name).toBe("Dairy Milk");
+  });
 
-    });
+  test("should return an error if any required field is missing", () => {
+    const shop = new SweetShop();
 
-    test("should return an error if any required field is missing",()=>{
+    // Test: Missing 'name'
+    // Expected: Should return an error stating all fields are required
+    let result = shop.addSweet(undefined, "Indian Sweet", 200, 3);
+    expect(result).toBeInstanceOf(Error);
+    expect(result.message).toBe("All Fields Are Required");
 
+    // Test: Missing 'category'
+    result = shop.addSweet("Kaju Katli", undefined, 200, 3);
+    expect(result).toBeInstanceOf(Error);
+    expect(result.message).toBe("All Fields Are Required");
 
-        const shop=new SweetShop();
+    // Test: Missing 'price'
+    result = shop.addSweet("Kaju Katli", "Indian Sweet", undefined, 3);
+    expect(result).toBeInstanceOf(Error);
+    expect(result.message).toBe("All Fields Are Required");
 
-        // Test: Missing 'name'
-       // Expected: Should return an error stating all fields are required
-        let result=shop.addSweet(undefined,"Indian Sweet",200,3);
-        expect(result).toBeInstanceOf(Error);
-        expect(result.message).toBe("All Fields Are Required");
-
-          // Test: Missing 'category'
-        result=shop.addSweet("Kaju Katli",undefined,200,3);
-        expect(result).toBeInstanceOf(Error);
-        expect(result.message).toBe("All Fields Are Required");
-  
-        // Test: Missing 'price'
-        result=shop.addSweet("Kaju Katli","Indian Sweet",undefined,3);
-       expect(result).toBeInstanceOf(Error);
-        expect(result.message).toBe("All Fields Are Required");
-
-        // Test: Missing 'quantity'
-        result=shop.addSweet("Kaju Katli","Indian Sweet",200,undefined);
-        expect(result).toBeInstanceOf(Error);
-        expect(result.message).toBe("All Fields Are Required");
-
-    });
+    // Test: Missing 'quantity'
+    result = shop.addSweet("Kaju Katli", "Indian Sweet", 200, undefined);
+    expect(result).toBeInstanceOf(Error);
+    expect(result.message).toBe("All Fields Are Required");
+  });
 
   // Test For the price less than zero of equal to zero
-    test("should return an error if the price of the sweet is less than zero",()=>{
-        const shop=new SweetShop();
-        let result=shop.addSweet("Kaju Katli","Indian Sweet",0,3);
-        expect(result).toBeInstanceOf(Error);
-        expect(result.message).toBe("The Price Has to be Greater Than 0");
+  test("should return an error if the price of the sweet is less than zero", () => {
+    const shop = new SweetShop();
+    let result = shop.addSweet("Kaju Katli", "Indian Sweet", 0, 3);
+    expect(result).toBeInstanceOf(Error);
+    expect(result.message).toBe("The Price Has to be Greater Than 0");
 
-        result=shop.addSweet("Kaju Katli","Indian Sweet",-50,3);
-        expect(result).toBeInstanceOf(Error);
-        expect(result.message).toBe("The Price Has to be Greater Than 0");
-    });
+    result = shop.addSweet("Kaju Katli", "Indian Sweet", -50, 3);
+    expect(result).toBeInstanceOf(Error);
+    expect(result.message).toBe("The Price Has to be Greater Than 0");
+  });
 
+  //test for the quantity value is greater than or equal to zero
 
-    //test for the quantity value is greater than or equal to zero
+  test("should return an error if the quantity is of the sweet is less than zero", () => {
+    const shop = new SweetShop();
+    let result = shop.addSweet("kaju Katli", "Indian Sweet", 200, -23);
+    expect(result).toBeInstanceOf(Error);
+    expect(result.message).toBe("The Quantity should be Greater than zero(0)");
+    result = shop.addSweet("kaju Katli", "Indian Sweet", 200, 0);
+    expect(result).toBeInstanceOf(Error);
+    expect(result.message).toBe("The Quantity should be Greater than zero(0)");
+  });
 
-    test("should return an error if the quantity is of the sweet is less than zero",()=>{
-        const shop=new SweetShop();
-        let result=shop.addSweet("kaju Katli","Indian Sweet",200,-23);
-        expect(result).toBeInstanceOf(Error);
-        expect(result.message).toBe("The Quantity should be Greater than zero(0)")
-        result=shop.addSweet("kaju Katli","Indian Sweet",200,0);
-        expect(result).toBeInstanceOf(Error);
-        expect(result.message).toBe("The Quantity should be Greater than zero(0)")
-        
-    })
+  // test for if the autoincrement is working fine or not
 
-
-      // test for if the autoincrement is working fine or not
-
-      test("should auto-increment ID for each added sweet", () => {
+  test("should auto-increment ID for each added sweet", () => {
     const shop = new SweetShop();
 
     shop.addSweet("Kaju Katli", "Indian Sweet", 200, 10);
@@ -80,60 +73,67 @@ describe("SweetShop-AddSweet",()=>{
     expect(sweets[1].id).toBe(2);
     expect(sweets[2].id).toBe(3);
   });
+});
 
-})
+describe("SweetShop-DeleteSweet", () => {
+  test("should delete a sweet by ID", () => {
+    const shop = new SweetShop();
+    //added the sweet to the sweets array
+    shop.addSweet("Kaju Katli", "Indian Sweet", 200, 10);
+    shop.addSweet("Dairy Milk", "Chocolate", 10, 20);
 
+    const result = shop.deleteSweet(1);
+    // expect output from the defined function
+    expect(result.length).toBe(1);
+    expect(result[0].id).toBe(2);
+  });
 
-describe("SweetShop-DeleteSweet",()=>{
+  test("should return the same list if Id to delete does not exist", () => {
+    const shop = new SweetShop();
 
-    test("should delete a sweet by ID",()=>{
-        const shop=new SweetShop();
-       //added the sweet to the sweets array
-        shop.addSweet("Kaju Katli","Indian Sweet",200,10);
-        shop.addSweet("Dairy Milk","Chocolate",10,20);
+    shop.addSweet("Kaju Katli", "Indian Sweet", 200, 10);
+    //test for the invalid id in the sweets array
+    const result = shop.deleteSweet(90);
 
-        const result=shop.deleteSweet(1);
-        // expect output from the defined function
-        expect(result.length).toBe(1);
-        expect(result[0].id).toBe(2);
+    expect(result).toBeInstanceOf(Error);
+    expect(result.message).toBe("Sweet with given ID not found");
+  });
+});
 
-    });
+describe("SweetShop-ViewSweet", () => {
+  test("should return a list of all available sweets", () => {
+    const shop = new SweetShop();
 
+    shop.addSweet("Kaju Katli", "Indian Sweet", 200, 10);
+    shop.addSweet("Dairy Milk", "Chocolate", 10, 20);
 
-    test("should return the same list if Id to delete does not exist",()=>{
-        const shop = new SweetShop();
+    const result = shop.viewSweet();
 
-        shop.addSweet("Kaju Katli","Indian Sweet",200,10);
-       //test for the invalid id in the sweets array
-        const result=shop.deleteSweet(90);
+    expect(result.length).toBe(2);
+    expect(result[0].name).toBe("Kaju Katli");
+    expect(result[1].name).toBe("Dairy Milk");
+  });
 
-         expect(result).toBeInstanceOf(Error);
-         expect(result.message).toBe("Sweet with given ID not found");
-    })
-})
+  //added the test to check when the list is empty
+  test("should return an empty array if no sweets are added", () => {
+    const shop = new SweetShop();
+    const result = shop.viewSweet();
 
+    expect(result).toEqual([]);
+  });
+});
 
-describe("SweetShop-ViewSweet",()=>{
-   
-     test("should return a list of all available sweets",()=>{
-        const shop=new SweetShop();
+describe("SweetShop-SearchSweet", () => {
+  //test for the search
+  test("should return sweets that match the given name", () => {
+    const shop = new SweetShop();
+    shop.addSweet("Kaju Katli", "Indian Sweet", 200, 10);
+    shop.addSweet("Rasmalai", "Indian Sweet", 150, 5);
+    shop.addSweet("Chocolate Bar", "Chocolate", 100, 8);
 
-        shop.addSweet("Kaju Katli","Indian Sweet",200,10);
-        shop.addSweet("Dairy Milk","Chocolate",10,20);
+    const result = shop.searchSweets({ name: "kaju" });
 
-        const result=shop.viewSweet();
-
-        expect(result.length).toBe(2);
-        expect(result[0].name).toBe("Kaju Katli");
-        expect(result[1].name).toBe("Dairy Milk");
-
-     });
- 
-     //added the test to check when the list is empty
-     test("should return an empty array if no sweets are added", () => {
-        const shop = new SweetShop();
-        const result = shop.viewSweet();
-
-        expect(result).toEqual([]);
-     });
-})
+    expect(result.length).toBe(1);
+    expect(result[0].name).toBe("Kaju Katli");
+  });
+});
