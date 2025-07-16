902e30b53d0888ac1152ac99af28725a9b6db347
Yashparmar2604 <yp842694@gmail.com>
2025-07-16 14:17:42 +0530

    implement the addsweet topass the initial test case

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ constructor(){
     this.id=0;
 }
 
+// Defines the addSweet function that validates input, generates a new sweet ID, and adds the sweet to the shop's inventory.
+ 
+addSweet(name,category,price,quantity){
+    this.id=this.id+1;
+    const newSweet={id:this.id,name,category,price,quantity};
+    this.sweets.push(newSweet);
+    return this.sweets;
+}
+
  }
 
 module.exports={SweetShop};
