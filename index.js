314bd1c8666aa4ec504c1ee49a23174deac6b698
Yashparmar2604 <yp842694@gmail.com>
2025-07-16 14:39:36 +0530

    implemented the missing required fields in addSweet

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ constructor(){
 // Defines the addSweet function that validates input, generates a new sweet ID, and adds the sweet to the shop's inventory.
  
 addSweet(name,category,price,quantity){
+
+
     this.id=this.id+1;
     const newSweet={id:this.id,name,category,price,quantity};
     this.sweets.push(newSweet);
