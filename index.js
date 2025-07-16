010281d02489ef5d57dea13963c43bdcddfc421e
Yashparmar2604 <yp842694@gmail.com>
2025-07-16 15:25:17 +0530

    implemented the price greater then zero check

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ constructor(){
  
 addSweet(name,category,price,quantity){
 
-    if(!name || !category || !price || !quantity){
+    if(!name || !category || price == undefined || quantity == undefined){
         return new Error("All Fields Are Required");
+    }
+
+    if(price<=0){
+        return new Error("The Price Has to be Greater Than 0");
     }
     this.id=this.id+1;
     const newSweet={id:this.id,name,category,price,quantity};
